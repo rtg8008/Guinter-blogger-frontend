caec5cc878413dc5f68efcc0daf4c0c0b5e71d2c
Ryan Guinter <421guinterr@gmail.com>
2022-06-28 19:02:31 -0600

    loads list from backend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React, { useEffect } from 'react'
 function App() {
+  const [names, setNames] = React.useState([{name: ''}])
+  React.useEffect(()=>{
+    const init = {
+
+    }
+    fetch(`https://crud-backend-ryan.herokuapp.com/`)
+    .then(res => res.json())
+    .then (data => {
+      setNames(data)
+      console.log(data)
+    });
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {names.map((e, i) => {
+        return <p>{e.name}</p>
+      })}
     </div>
   );
 }
